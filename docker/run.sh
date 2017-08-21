4e7dc80765339125fadd5c2061b3941ce6b70bb5
romansky <romansky@ualberta.ca>
2017-08-21 13:37:48 -0600

    Added a Dockerfile and script to run it.

---
+++ docker/run.sh
@@ -0,0 +1,4 @@
+# TODO get docker to launch daemon automatically? It can be done with manual commands
+# TODO get docker to setup a dataset in the container?
+# TODO get docker to setup a queryset in the container?
+docker run -it -p 4568:4567 local-scc .
