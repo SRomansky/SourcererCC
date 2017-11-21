5d33e284d4f7e476f02d2e963921c075bb280274
romansky <romansky@ualberta.ca>
2017-11-21 14:47:54 -0700

    possibly fixed the break.

--- clone-detector/src/com/mondego/httpcommunication/Query.java
+++ clone-detector/src/com/mondego/httpcommunication/Query.java
@@ -87,9 +87,7 @@ public class Query {
 		 */
 
 		// TODO check if the client is in the IDLE state
-		TASK_EXECUTOR.submit(new Runnable() {
-            @Override
-            public void run() {
+		
 		MultivaluedMap metaDataMap = metaData.getValueAs(MultivaluedMap.class);
 		String qid = (String) ((java.util.LinkedList) metaDataMap.get("qid")).get(0);  // why
 
@@ -147,6 +145,7 @@ public class Query {
 		}
 		/* unpack contents from manager into query directory */
 		
+		
 		// TODO check if the daemon is busy in a query
 		// TODO if the daemon is busy there is a bug :)
 		// TODO (later) if the daemon is busy, send a POST with an error to results/{id} on the manager
@@ -165,6 +164,9 @@ public class Query {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		TASK_EXECUTOR.submit(new Runnable() {
+            @Override
+            public void run() {
 		
 		/* run the query */
 		// TODO check if daemon status is loaded
